1a52bc99e49db95b7b0fff9a63a12891bf94da5f
hojin <seedevk8s@gmail.com>
2021-11-01 06:31:40 +0900

    first commit

---
+++ src/kr/co/ezenac/step01/DoubleLinkedList.java
@@ -0,0 +1,18 @@
+package kr.co.ezenac.step01;
+
+public class DoubleLinkedList<T> {
+    public Node<T> head = null;
+    public Node<T> tail = null;
+
+    public class Node<T> {
+        T data;
+        Node<T> prev = null;
+        Node<T> next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    
+}
